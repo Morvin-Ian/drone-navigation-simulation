e77e90789198cc931fd6d5dfa2d6b9bbdc7ceabc
OluochIan <morvinian@gmail.com>
2024-08-22 06:19:13 +0300

    Added feature: Edited Models Plural Names

--- navigate/models.py
+++ navigate/models.py
@@ -3,11 +3,14 @@ from django.contrib.gis.db import models as gis_models
 
 
 class HealthFacilities(models.Model):
-    name = models.CharField(max_length=80, null=True, blank=True)
+    name    = models.CharField(max_length=80, null=True, blank=True)
     healthcare = models.CharField(max_length=167, null=True, blank=True)
     amenity = models.CharField(max_length=80, null=True, blank=True)
     operatorty = models.CharField(max_length=80, null=True, blank=True)
     geom = gis_models.PointField(srid=4326)
+    
+    class Meta:
+        verbose_name_plural = "Health Facilities"
 
     def __str__(self) -> str:
         return self.name if self.name else "Unnamed Facility"
@@ -19,6 +22,9 @@ class Drones(models.Model):
     battery_capacity = models.IntegerField(default=100)
     current_charge = models.IntegerField(default=100)
     geom = gis_models.PointField(srid=4326)
+    
+    class Meta:
+        verbose_name_plural = "Drones"
     
     def __str__(self) -> str:
         return self.name
